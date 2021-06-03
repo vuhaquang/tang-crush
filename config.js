1bf2c9ab87cafdc1ea6e87b731e20d81530a40da
Vượng <59494651+vuongprogk@users.noreply.github.com>
2021-06-04 00:13:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Gửi cho người đáng tin nhất thế giới :>',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
